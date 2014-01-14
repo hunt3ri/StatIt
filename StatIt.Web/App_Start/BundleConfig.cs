20203f74e7af2eab409d27ed768488d8eb203eb4
Iain Hunter <iain.hunter@outlook.com>
2014-01-14 22:38:08 +0000

    Add bootstrap js dependencies

--- StatIt.Web/App_Start/BundleConfig.cs
+++ StatIt.Web/App_Start/BundleConfig.cs
@@ -14,6 +14,14 @@ namespace StatIt.Web.App_Start
             bundles.Add(new StyleBundle("~/Content/css").Include(
                      "~/Content/bootstrap.css",
                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                     "~/Scripts/bootstrap.js",
+                     "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-{version}.js"));
+
         }
     }
 }
