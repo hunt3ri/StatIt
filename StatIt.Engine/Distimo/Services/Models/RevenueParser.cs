f258ffc19efcd0f175d61eb155b9b3b139a6ac4c
Iain Hunter <iain.hunter@outlook.com>
2014-02-05 22:40:47 +0000

    Rename file

---
+++ StatIt.Engine/Distimo/Services/Models/RevenueParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatIt.Engine.Distimo.Services.Models
+{
+    /// <summary>
+    /// Helper class used to help parse raw revenue data from Distimo
+    /// </summary>
+    public class RevenueParser
+    {
+        public DateTime OldestDate { get; set; }
+        public int MaxPointCount { get; set; }
+
+        public Dictionary<string, List<int>> RawRevenueData { get; set; }
+        public Dictionary<string, List<int>> CleanRevenueData { get; set; }
+
+
+        public RevenueParser()
+        {
+            OldestDate = DateTime.Now;
+            RawRevenueData = new Dictionary<string, List<int>>();
+            CleanRevenueData = new Dictionary<string, List<int>>();
+
+        }
+    }
+}
