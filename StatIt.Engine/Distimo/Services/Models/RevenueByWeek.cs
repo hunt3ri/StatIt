296a7fa271f9229d2193b900ac8a5524c946cb72
Iain Hunter <iain.hunter@outlook.com>
2014-02-05 22:36:40 +0000

    Rename file

---
+++ StatIt.Engine/Distimo/Services/Models/RevenueByWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatIt.Engine.Distimo.Services.Models
+{
+    public class RevenueByWeek
+    {
+        public string Week { get; set; }
+        public int Amazon { get; set; }
+        public int Apple { get; set; }
+        public int Google { get; set; }
+    }
+}
